75719f8430c20653f15915f425b5fd4d751ab312
akallor <ashwinkallor@gmail.com>
2025-03-12 14:35:40 +0100

    Update queries.sh

--- queries.sh
+++ queries.sh
@@ -7,3 +7,7 @@ grep '\bNF1\b' *.bed | grep 'splicing' | egrep 'Pathogenic|pathogenic' | awk '{p
 #Extract nucleotide sequences from the bed file to prepare for Transdecoder
 
 bedtools getfasta -fi ../../genome/hg38.fa -bed clinvar_NF1_pathogenic.bed -fo extracted_NF1_clinvar_sequences.fa
+
+#Get all splice-site variants from all the filtered and annotated txt files in all samples
+
+grep '\bNF1\b' JH-2-*/*.hg38_multianno.txt | grep '\bsplicing\b' | egrep 'Pathogenic|pathogenic' | awk -F':' '{print $2,$6}
