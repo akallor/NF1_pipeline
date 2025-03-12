540e538665274330a99f19775335981c5cb0e4de
akallor <ashwinkallor@gmail.com>
2025-03-12 11:29:56 +0100

    Update queries.sh

--- queries.sh
+++ queries.sh
@@ -3,3 +3,7 @@
 #Updated query
 
 grep '\bNF1\b' *.bed | grep 'splicing' | egrep 'Pathogenic|pathogenic' | awk '{print $13, $14, $15,$6,$7,$12,$8, $9, $2, $3}' OFS="\t" > clinvar_NF1_pathogenic.bed
+
+#Extract nucleotide sequences from the bed file to prepare for Transdecoder
+
+bedtools getfasta -fi ../../genome/hg38.fa -bed clinvar_NF1_pathogenic.bed -fo extracted_NF1_clinvar_sequences.fa
