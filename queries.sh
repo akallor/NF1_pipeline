74bf75ad989a87085aee587c4373c6caafc6e33c
akallor <ashwinkallor@gmail.com>
2025-03-12 10:38:27 +0100

    Updated query for pathogenic splice variant extraction.

--- queries.sh
+++ queries.sh
@@ -1,3 +1,5 @@
 #Identity all the clinically pathogenic/likely pathogenic splicing variants from the files
 
-grep '\bNF1\b' *.bed | grep 'splicing' | grep 'pathogenic' > clinvar_NF1_pathogenic_splicing_variants.bed
+#Updated query
+
+grep '\bNF1\b' *.bed | grep 'splicing' | egrep 'Pathogenic|pathogenic' | awk '{print $13, $14, $15,$6,$7,$12,$8, $9, $2, $3}' OFS="\t" > clinvar_NF1_pathogenic.bed
