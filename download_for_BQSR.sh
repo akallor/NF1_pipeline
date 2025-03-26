1f250bbcb629071fd94feede40780fe560289802
akallor <ashwinkallor@gmail.com>
2025-03-26 19:01:29 +0100

    Create download_for_BQSR.sh

---
+++ download_for_BQSR.sh
@@ -0,0 +1,8 @@
+Download the latest hg38 dbSNP vcf file:
+
+wget https://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/common_all_20180418.vcf.gz 
+
+Download the other vcf files:
+
+wget https://console.cloud.google.com/storage/browser/_details/genomics-public-data/resources/broad/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+wget https://console.cloud.google.com/storage/browser/_details/genomics-public-data/resources/broad/hg38/v0/1000G_phase1.snps.high_confidence.hg38.vcf.gz
